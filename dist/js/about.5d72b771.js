d8654dd46bf580113beed93934ac6728b83c07d8
Iason Argyrakis <jasonargirakis@gmail.com>
2022-07-28 00:57:24 +0300

    Deployed to GitHub Pages

---
+++ dist/js/about.5d72b771.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkweb_pass"]=self["webpackChunkweb_pass"]||[]).push([[443],{1272:function(s,n,t){t.r(n),t.d(n,{default:function(){return i}});var u=function(){var s=this;s._self._c;return s._m(0)},e=[function(){var s=this,n=s._self._c;return n("div",{staticClass:"about"},[n("h1",[s._v("This is an about page")])])}],a=t(1001),l={},r=(0,a.Z)(l,u,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.5d72b771.js.map
