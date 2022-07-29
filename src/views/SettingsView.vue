8ba0a37b082c28c656769226e48140cdea3bd0e1
Iason Argyrakis <jasonargirakis@gmail.com>
2022-07-29 03:09:18 +0300

    SettingsView.vue

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <settings-component/>
+</template>
+
+<script>
+  import SettingsComponent from "@/components/SettingsComponent";
+
+  export default {
+    name: 'SettingsView',
+
+    components: {
+      SettingsComponent,
+    },
+  }
+</script>
