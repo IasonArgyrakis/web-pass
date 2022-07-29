f8a04026e566d7e058e5ff7375a1d720133d15a0
Iason Argyrakis <jasonargirakis@gmail.com>
2022-07-29 03:09:28 +0300

    SettingsView.vue

---
+++ src/components/SettingsComponent.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="settings-component">
+    <h1>settings-component Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'settings-component',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .settings-component {
+
+  }
+</style>
